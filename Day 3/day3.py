27d4e897ee452aad07541c54b46901093be2aa34
MichaelPate <pate5800@gmail.com>
2020-12-15 11:48:44 -0700

    Day 3 complete and working

--- Day 3/day3.py
+++ Day 3/day3.py
@@ -1,7 +1,14 @@
 def main():
     lines = open('day3Input.txt').readlines()
 
-    print(computePath(lines, 3, 1))
+    product = 1
+    product *= computePath(lines, 1, 1)
+    product *= computePath(lines, 3, 1)
+    product *= computePath(lines, 5, 1)
+    product *= computePath(lines, 7, 1)
+    product *= computePath(lines, 1, 2)
+    print(product)
+    #print(computePath(lines, 3, 1))
 
 
 def computePath(array, colInc, rowInc):
