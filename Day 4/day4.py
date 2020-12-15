ac46cd956e2c23b5980a84ad68192b131d466c6f
MichaelPate <pate5800@gmail.com>
2020-12-15 12:00:48 -0700

    Part 1 working and complete

--- Day 4/day4.py
+++ Day 4/day4.py
@@ -2,12 +2,22 @@ def main():
     with open("day4Input.txt") as f:
         lines = f.read()
     passports = lines.split("\n\n")
+    fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+    validCount = 0
     
     for p in passports:
         p = p.replace('\n', ' ')
+        # cid is optional, the rest are not
         print(p)
+        hasAll = True
+        for field in fields:
+            if field not in p:
+                hasAll = False
+                break
+        if hasAll: validCount += 1
+    
         print("\n")
-
+    print(validCount)
 
 if __name__=='__main__':
     main()
