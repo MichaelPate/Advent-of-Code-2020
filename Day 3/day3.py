c9396a3410764551f52add202a22e773359df29c
MichaelPate <pate5800@gmail.com>
2020-12-15 11:47:01 -0700

    Increased modularity for part 2

--- Day 3/day3.py
+++ Day 3/day3.py
@@ -1,23 +1,19 @@
 def main():
     lines = open('day3Input.txt').readlines()
 
-    lineLength = 0
-    treeCount = 0
-    row = 0
-    column = 0
-    stringPos = 0
+    print(computePath(lines, 3, 1))
 
-    for line in lines:
-        line = line.split('\n')[0]
-        lineLength = len(line)
-        
-        # Adapt column into stringPos
-        stringPos = column % lineLength
-        #print(str(stringPos) + "\t" + line[stringPos])
-        if line[stringPos] == '#': treeCount += 1
-        column += 3
-    
-    print(treeCount)
+
+def computePath(array, colInc, rowInc):
+    row = 0
+    col = 0
+    count = 0
+    while row < len(array):
+        line = array[row].split('\n')[0]
+        if line[col % len(line)] == '#': count += 1
+        row += rowInc
+        col += colInc
+    return count
 
 
 if __name__=='__main__':
