98282f036f4cc04a40485c911a0d4672599e76e8
MichaelPate <pate5800@gmail.com>
2020-12-15 11:22:51 -0700

    Part 1 complete

--- Day 3/day3.py
+++ Day 3/day3.py
@@ -1,8 +1,24 @@
 def main():
     lines = open('day3Input.txt').readlines()
+
+    lineLength = 0
+    treeCount = 0
+    row = 0
+    column = 0
+    stringPos = 0
+
     for line in lines:
         line = line.split('\n')[0]
-        print(line)
+        lineLength = len(line)
+        
+        # Adapt column into stringPos
+        stringPos = column % lineLength
+        #print(str(stringPos) + "\t" + line[stringPos])
+        if line[stringPos] == '#': treeCount += 1
+        column += 3
+    
+    print(treeCount)
+
 
 if __name__=='__main__':
     main()
